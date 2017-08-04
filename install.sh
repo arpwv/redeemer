14c20a8fded7a750ff44f12c44eff7a4d0cb1860
John Gerlock <john.gerlock@gmail.com>
2017-08-03 21:43:00 -0400

    Updated Pipfile

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ ssh -A ubuntu@${EC2_HOST} << SSHCMD
 set -e
 sudo apt-get update
 sudo apt-get install python3-pip libssl-dev
-rm -rf ~/redeemer
-rm -f /etc/cron.daily/de_delegate_cron.sh
+sudo rm -rf ~/redeemer
+sudo rm -f /etc/cron.daily/de_delegate_cron.sh
 git clone git@github.com:steemit/redeemer.git
 cd redeemer
 pip3 install pipenv
 export PIPENV_VENV_IN_PROJECT=1
-pipenv install --three
-sudo chown -R root ${SRC_DIR}
-sudo chmod -R 0755  ${SRC_DIR}
-sudo echo "cd ${SRC_DIR} && pipenv run python ./dedelegate.py --no_broadcast=True"  > /etc/cron.daily/dedelegate.sh
+pipenv install
+echo 'cd /home/ubuntu/redeemer && pipenv run python ./dedelegate.py --no_broadcast=True'  > dedelegate.sh
+sudo cp dedelegate.sh /etc/cron.daily
+sudo chmod -R 0700  ${SRC_DIR}
 SSHCMD
 
