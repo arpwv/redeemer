71a5aabb74f145ff127a80751f7d2ba02bc4a433
roadscape <roadscape@users.noreply.github.com>
2018-01-08 23:50:24 -0600

    edge case

--- redeemer/delegator.py
+++ redeemer/delegator.py
@@ -55,17 +55,22 @@ class Delegator(object):
       else:
         new_delegated_vests = max(0, self.TARGET_VESTS - account_vests)
 
+      # do not process attempted increases in delegation
+      if new_delegated_vests > old_delegated_vests:
+        return None
+
       # if target vests are below minimum vests, round up.
       if new_delegated_vests > 0 and new_delegated_vests < self.MIN_VESTS:
         new_delegated_vests = self.MIN_VESTS
 
-      delta = new_delegated_vests - old_delegated_vests
+      # theoretically an account could have a delegation below the min delta;
+      #   to reset it, we must /raise/ it above the bar first.
+      elif new_delegated_vests == 0 and old_delegated_vests < self.MIN_VESTS_DELTA:
+        new_delegated_vests = max(self.MIN_VESTS, old_delegated_vests + self.MIN_VESTS_DELTA)
 
+      delta = new_delegated_vests - old_delegated_vests
       if abs(delta) < self.MIN_VESTS_DELTA:
           return None # blockchain-enforced minimum delta
-
-      if delta > 0:
-          return None # do not increase steemit delegation
 
       return {'name': name,
               'shares': acct['vesting_shares'],
